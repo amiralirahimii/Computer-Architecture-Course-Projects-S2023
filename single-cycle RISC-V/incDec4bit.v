00d0f9fca5d7bda1b782965cdf6b4445a8dc7a39
amiralirahimii <140163598+amiralirahimii@users.noreply.github.com>
2023-07-21 14:45:39 +0330

    Delete single-cycle RISC-V directory

--- single-cycle RISC-V/incDec4bit.v
+++
@@ -1,6 +0,0 @@
-module incDec4bit(dataIn, incDec, dataOut);
-	input[3:0] dataIn;
-	input incDec;
-	output [3:0] dataOut;
-	assign dataOut = (incDec==1) ? (dataIn+1) : (dataIn-1);
-endmodule
--- single-cycle RISC-V/mux2to1_4bit.v
+++
@@ -1,6 +0,0 @@
-module mux2to1_4bit(i0, i1, sel, out);
-	input[3:0] i0, i1;
-	input sel;
-	output [3:0]out;
-	assign out = (sel==0) ? i0 : i1;
-endmodule
